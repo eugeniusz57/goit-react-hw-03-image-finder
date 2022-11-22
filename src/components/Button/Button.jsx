d5bcc9060f1b8b8922eaee1d53dbea9f23a8dec9
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-11-22 16:52:29 +0100

    Prettier

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,12 @@
-import { Button } from "./Button.styled";
-import PropTypes from 'prop-types'; 
+import { Button } from './Button.styled';
+import PropTypes from 'prop-types';
 
+export const ButtonLoadMore = ({ onClick }) => (
+  <Button type="button" onClick={onClick}>
+    Loade more
+  </Button>
+);
 
-export const ButtonLoadMore = ({onClick}) => 
-<Button type="button" onClick={onClick}>
-  Loade more
-    </Button>
-    ButtonLoadMore.propTypes = {
-      onClick: PropTypes.func.isRequired,
-        }
+ButtonLoadMore.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
--- src/components/ImageGalleryItems/ImageGalleryItems.jsx
+++ src/components/ImageGalleryItems/ImageGalleryItems.jsx
@@ -1,14 +1,24 @@
-import { ImageGalleryItem, ImageGalleryItemImage } from "./ImageGalleryItems.styled";
-import PropTypes from 'prop-types'; 
+import {
+  ImageGalleryItem,
+  ImageGalleryItemImage,
+} from './ImageGalleryItems.styled';
 
-export const ImageGalleryItems = ({webformatURL, largeImageURL, onClick}) => <ImageGalleryItem >
-<ImageGalleryItemImage src={webformatURL} alt={largeImageURL}  onClick={() => {
-          onClick(largeImageURL);
-        }}/>
-</ImageGalleryItem> 
+import PropTypes from 'prop-types';
+
+export const ImageGalleryItems = ({ webformatURL, largeImageURL, onClick }) => (
+  <ImageGalleryItem>
+    <ImageGalleryItemImage
+      src={webformatURL}
+      alt={largeImageURL}
+      onClick={() => {
+        onClick(largeImageURL);
+      }}
+    />
+  </ImageGalleryItem>
+);
 
 ImageGalleryItems.propTypes = {
   webformatURL: PropTypes.string.isRequired,
   largeImageURL: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-}
+};
